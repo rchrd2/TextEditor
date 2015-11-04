98b65e7e2945882901a93a582a67be107eb0e2ff
rchrd2 <me@rchrd.net>
2015-11-04 00:40:21 -0800

    Fix various issue with css. Clean up code.

--- texteditor.js
+++ texteditor.js
@@ -4,30 +4,30 @@ Data = new Mongo.Collection("Data");
 /* Important, do not change DATA_ID or else all apps will lose their data */
 DATA_ID = 'default_id';
 
+/* The default CSS for new documents */
+DEFAULT_CSS_FONT_SIZE = "16px";
+DEFAULT_CSS_FONT_FAMILY = "Menlo, monospace";
+DEFAULT_CSS = `font-family: ${DEFAULT_CSS_FONT_FAMILY};
+font-size: ${DEFAULT_CSS_FONT_SIZE};
+background-color: #FFFFFF;
+color: #000000;
+`;
+
 function initClient () {
   var _hasPermissionCache = false;
 
   Meteor.startup(function () {
-    // Asynchronously check permission on startup
+    /* Asynchronously check permission on startup */
     Meteor.call('hasPermission', (error, result) => {
       _hasPermissionCache = result;
     });
 
     Session.set({
       "isCssOpened": false,
-      "fontSize": "16px",
-      "fontFamily": "monospace",
+      "fontSize": DEFAULT_CSS_FONT_SIZE,
+      "fontFamily": DEFAULT_CSS_FONT_FAMILY
     });
   });
-
-  Template.texteditor.rendered = function () {
-    //  document.createElement('textarea');
-    var ta = document.getElementById('textarea');
-    Session.set({
-      "fontSize": ta.style.fontSize,
-      "fontFamily": ta.style.fontFamily,
-    })
-  };
 
   Template.texteditor.helpers({
     doc: () => Data.findOne(DATA_ID),
@@ -39,59 +39,73 @@ function initClient () {
   });
 
   Template.texteditor.events({
-    "input #textarea": function (event, template) {
-      Data.update(DATA_ID, {$set: { value: event.target.value }});
+    "input #textarea": function (e, template) {
+      Data.update(DATA_ID, {$set: { value: e.target.value }});
     },
 
-    "input #css-textarea": function (event, template) {
-      Data.update(DATA_ID, {$set: { css: event.target.value }});
-    },
-
-    "click #collapse-button": function (event, template) {
+    "click #collapse-button": function (e, template) {
       Session.set("isCssOpened", ! Session.get("isCssOpened"));
-      event.preventDefault();
+      e.preventDefault();
       return false;
     },
 
-    "click #toggle-light-button": function (event, template) {
+    /* Handle raw css editing */
+    "input #css-textarea": function (e, template) {
       var ta = document.getElementById('textarea');
+      ta.style.cssText = e.target.value;
+      Data.update(DATA_ID, {$set: { css: e.target.value }});
+      updateSessionFromStyleChange(ta);
+    },
+
+    /* Swap color and background-color */
+    "click #toggle-light-button": function (e, template) {
+      e.preventDefault();
+      var ta = document.getElementById("textarea");
       var color = ta.style.color || "#000000";
       var bgColor = ta.style.backgroundColor || "#FFFFFF";
       ta.style.backgroundColor = color;
       ta.style.color = bgColor;
       Data.update(DATA_ID, {$set: { css: ta.style.cssText }});
-      event.preventDefault();
-      return false;
+      updateSessionFromStyleChange(ta)
     },
 
+    /* Update font-family from selection */
     "change select[name=font-family]": function (e, template) {
-      var ta = document.getElementById('textarea');
+      e.preventDefault();
+      var ta = document.getElementById("textarea");
       var value = e.currentTarget.options[e.currentTarget.selectedIndex].value;
       ta.style.fontFamily = value;
-      Session.set({
-        "fontSize": ta.style.fontSize,
-        "fontFamily": ta.style.fontFamily,
-      });
       Data.update(DATA_ID, {$set: { css: ta.style.cssText }});
-      event.preventDefault();
-      return false;
+      updateSessionFromStyleChange(ta);
     },
 
+    /* Update font-size from selection */
     "change select[name=font-size]": function (e, template) {
-      var ta = document.getElementById('textarea');
+      e.preventDefault();
+      var ta = document.getElementById("textarea");
       ta.style.fontSize = e.currentTarget.options[e.currentTarget.selectedIndex].value;
-      Session.set({
-        "fontSize": ta.style.fontSize,
-        "fontFamily": ta.style.fontFamily,
-      });
       Data.update(DATA_ID, {$set: { css: ta.style.cssText }});
-      event.preventDefault();
-      return false;
+      updateSessionFromStyleChange(ta);
     }
   });
 
-  // Register these exported collections
-  Meteor.subscribe("thedocument");
+  /* Session needs to be updated for the form. Also update body bg-color */
+  var updateSessionFromStyleChange = function (el) {
+    Session.set({
+      "fontSize": el.style.fontSize || DEFAULT_CSS_FONT_SIZE,
+      "fontFamily": el.style.fontFamily || DEFAULT_CSS_FONT_FAMILY,
+    });
+    document.body.style.backgroundColor = el.style.backgroundColor || null;
+  }
+
+  /* Register these exported collections */
+  Meteor.subscribe("thedocument", function() {
+    /* Init style from doc css */
+    var doc = Data.findOne(DATA_ID);
+    var el = document.getElementById("textarea");
+    el.style.cssText = doc.css;
+    updateSessionFromStyleChange(el);
+  });
 }
 
 function initServer () {
@@ -111,12 +125,12 @@ function initServer () {
       var id = Data.insert({
         _id: DATA_ID,
         value: "",
-        css: `font-family: Menlo, monospace;
-font-size: 16px;
-background-color: #FFFFFF;
-color: #000000;
-`,
+        css: DEFAULT_CSS,
       });
+    }
+    /* Init css for upgraded documents */
+    if ( ! doc.hasOwnProperty('css')) {
+      Data.update(DATA_ID, {$set: { css: DEFAULT_CSS }});
     }
   });
 
@@ -131,10 +145,7 @@ color: #000000;
   });
 
   Meteor.methods({
-    /**
-     * Note it's important to use an old-school function() {}, so `this` gets
-     * bound correctly
-     */
+    /* Note function() {}, so `this` gets bound correctly */
     hasPermission: function () {
       var h = headers.get(this);
       var p = h['x-sandstorm-permissions'] || "";
